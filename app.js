8f036d0fce089867ce607a10b7bb8de3a50b2015
chaseunghyeok <xvds1@naver.com>
2023-09-13 10:23:37 +0900

    SEUNGHYUK_2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const express = require("express");
 const cors = require("cors");
-const 
+const bcrypt = require("bcrypt")
 const dotenv = require("dotenv").config();
 const morgan = require("morgan");
 
@@ -16,14 +16,14 @@ app.use(express.json());
 
 const userServices = require("./services/userServices");
 
-// const myDataSource = new DataSource({
-//   type: process.env.TYPEORM_CONNECTION,
-//   host: process.env.TYPEORM_HOST,
-//   port: process.env.TYPEORM_PORT,
-//   username: process.env.TYPEORM_USERNAME,
-//   password: process.env.TYPEORM_PASSWORD,
-//   database: process.env.TYPEORM_DATABASE,
-// });
+const AppDataSource = new DataSource({
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+});
 
 // 실행
 app.get("/", userServices.welcome); // 메인홈
@@ -31,7 +31,10 @@ app.get("/users", userServices.getUsers); // 유저데이터 화면
 app.post("/users", userServices.createUsers); // 회원가입
 app.post("/login", userServices.login); //  로그인 - ing
 
-// const test = myDataSource.initialize()
+app.get("/board", userServices.showPost);
+app.post("/board", userServices.addPost)
+
+// const test = AppDataSource.initialize()
 //     .then(() => {
 //         console.log("Data Source has been initialized!")
 //     })
@@ -50,8 +53,9 @@ const start = async () => {
   }
 };
 
-userServices.myDataSource.initialize().then(() => {
+userServices.AppDataSource.initialize().then(() => {
   console.log("Data Source has been initialized!");
 });
+
 
 start();
